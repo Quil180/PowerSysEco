5e47024ed95b755d754a7e4d92ce1d6ae9b7f286
Quil <quil180gaming@gmail.com>
2025-09-11 00:10:05 -0400

    finished hw2

--- HW/hw2/hw2.py
+++ HW/hw2/hw2.py
@@ -1,29 +1,91 @@
 import sympy as sp
+import pandas as pd
+import numpy as np
 
-p, q = sp.symbols('p q')
+# defining symbolic variables for the equations
+q_sym, pi_sym = sp.symbols("q pi")
 
-# Given these...
-p_demand = 2000 - 10*q
-q_supply = 0.2 * p - 40
-q_demand = 200 - 0.1 * p
+# defining the list of demand values
+q_values = [0, 50, 100, 150, 200]
 
-# Solve for the price and quantity equilibriums
-p_eq = sp.solve(sp.Eq(q_supply, q_demand), p)[0]
-q_eq = q_supply.subs(p, p_eq)
+# Demand curve 1, q = 200 - pi
+print("Verifying Demand Curve 1: q = 200 - pi")
 
-# Find the Gross Consumers' Surplus, Net Consumers' Surplus, and Revenue, and Producers' Surplus, and Global Surplus...
-GCS = sp.integrate(p_demand, (q, 0, q_eq))
-NCS = GCS - (p_eq * q_eq)
-R = p_eq * q_eq
-p_min = sp.solve(sp.Eq(q_supply, 0), p)[0]
-PS = 0.5 * q_eq * (p_eq - p_min)
-W = NCS + PS
+# defining the demand equation
+demand_eq1 = sp.Eq(q_sym, 200 - pi_sym)
 
-# Printing the output.
-print("Equilibrium Price ($/Unit) =", float(p_eq))
-print("Equilibrium Quantity (in Units) =", float(q_eq))
-print("Consumers' Gross Surplus (Dollars) =", float(GCS))
-print("Consumers' Net Surplus (Dollars) =", float(NCS))
-print("Producers' Revenue (Dollars) =", float(R))
-print("Producers' Profit (Dollars) =", float(PS))
-print("Global Welfare (Dollars) =", float(W))
+# solving for pi to get the inverse demand function
+pi_expr1 = sp.solve(demand_eq1, pi_sym)[0]
+
+# now we differentiate q with respect to pi to find dq/dpi
+dq_dpi1 = sp.diff(200 - pi_sym, pi_sym)
+
+# creating the elasticity formula
+epsilon_expr1 = (dq_dpi1) * (pi_sym / q_sym)
+
+# calculating the results for each q value
+results1 = []
+for q_val in q_values:
+    if q_val == 0:
+        pi_val = 200
+        # if elasticity is undefined (approaches -infinity)
+        epsilon_val = -np.inf
+    else:
+        # substitute q to find pi
+        pi_val = pi_expr1.subs(q_sym, q_val)
+        # substitute pi and q to find elasticity
+        epsilon_val = epsilon_expr1.subs([(pi_sym, pi_val), (q_sym, q_val)])
+
+    results1.append(
+        {
+            "Demand (q)": q_val,
+            "Price (pi)": pi_val,
+            "Elasticity (epsilon)": float(epsilon_val),
+        }
+    )
+
+# display these results in a table
+df1 = pd.DataFrame(results1)
+print(df1.to_string(index=False))
+print("\n" + "=" * 50 + "\n")
+
+
+# Demand Curve 2: q = 10000 / pi
+print("Verifying Demand Curve 2: q = 10000 / pi")
+
+# defining the demand equation
+demand_eq2 = sp.Eq(q_sym, 10000 / pi_sym)
+
+# solving for pi
+pi_expr2 = sp.solve(demand_eq2, pi_sym)[0]
+
+# differentiating q with respect to pi
+dq_dpi2 = sp.diff(10000 / pi_sym, pi_sym)
+
+# create the elasticity formula
+epsilon_expr2 = (dq_dpi2) * (pi_sym / q_sym)
+# sympy can simplify this for me
+epsilon_simplified2 = sp.simplify(epsilon_expr2)
+
+results2 = []
+for q_val in q_values:
+    if q_val == 0:
+        pi_val = np.inf
+        # if elasticity is constant at -1, even at the limit
+        epsilon_val = -1
+    else:
+        pi_val = pi_expr2.subs(q_sym, q_val)
+        # the elasticity is always -1, but im going to calculate it for consistency
+        epsilon_val = epsilon_expr2.subs([(pi_sym, pi_val), (q_sym, q_val)])
+
+    results2.append(
+        {
+            "Demand (q)": q_val,
+            "Price (pi)": float(pi_val),
+            "Elasticity (epsilon)": int(epsilon_val),
+        }
+    )
+
+# display these results in a table
+df2 = pd.DataFrame(results2)
+print(df2.to_string(index=False))
--- shell.nix
+++ shell.nix
@@ -1,21 +1,18 @@
 # shell.nix for setting up development environment for Nixos/Nix
-with (import <nixpkgs> {}); let
-  my-python = python-packages:
-    with python-packages; [
-      find-libpython
-      pip
-    ];
-  python-with-my-packages = python3.withPackages my-python;
-in
+with (import <nixpkgs> {});
   mkShell {
     buildInputs = [
-      python-with-my-packages
-      libgcc
-      python312Packages.pip
+			(pkgs.python3.withPackages (ps: [
+				ps.pip
+				ps.pandas
+				ps.numpy
+				ps.sympy
+				# Add other packages here
+			]))
     ];
-    shellHook = ''
-			python3 -m venv .sympy
-			source .sympy/bin/activate
-			pip3 install sympy
-    '';
+			 # shellHook = ''
+			 # python3 -m venv .sympy
+			 # source .sympy/bin/activate
+			 # pip3 install sympy pandas numpy
+			 # '';
   }
